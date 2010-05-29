0845cb5f1a4aa43c0c839303a80bf033fd1aef27
Dan Mohl <dmohl@yahoo.com>
2010-05-29 15:52:50 -0500

    Refactored

--- src/FSharpCouch/FSharpCouch.fs
+++ src/FSharpCouch/FSharpCouch.fs
@@ -1,4 +1,4 @@
-﻿// This is a partial port of SharpCouch
+﻿// This is a simple .NET API for CouchDB loosely based on SharpCouch
 // http://code.google.com/p/couchbrowse/source/browse/trunk/SharpCouch/SharpCouch.cs
 module FSharpCouch
     open System
@@ -16,50 +16,40 @@ module FSharpCouch
         use requestStream = request.GetRequestStream()
         requestStream.Write(bytes, 0, bytes.Length) 
         request
-    let AyncReadResponse (request:WebRequest) =
+    let AsyncReadResponse (request:WebRequest) =
         async { use! response = request.AsyncGetResponse()
                 use stream = response.GetResponseStream()
                 use reader = new StreamReader(stream)
                 let contents = reader.ReadToEnd()
                 return contents }
     let ProcessPostRequest url methodName content contentType =
-        async { let request = WriteRequest url methodName content contentType
-                let! response = AyncReadResponse(request) 
-                return response}
+        WriteRequest url methodName content contentType 
+        |> AsyncReadResponse
         |> Async.RunSynchronously
     let ProcessPutOrDeleteRequest url methodName =
-        async { let request = WebRequest.Create(string url)
-                request.Method <- methodName
-                let! response = AyncReadResponse(request) 
-                return response}
-        |> Async.RunSynchronously
+        try
+            let request = WebRequest.Create(string url)
+            request.Method <- methodName
+            AsyncReadResponse request 
+            |> Async.RunSynchronously
+        with
+        | e -> failwith e.Message
     let ProcessGetRequest url =
-        async { let request = WebRequest.Create(string url)
-                let! response = AyncReadResponse(request) 
-                return response}
+        WebRequest.Create(string url)
+        |> AsyncReadResponse
         |> Async.RunSynchronously
     let BuildUrl (server:string) (database:string) =
         server + "/" + database.ToLower()
     let CreateDatabase server database =
-        try
-            ProcessPutOrDeleteRequest (BuildUrl server database) "PUT"
-        with
-        | e -> failwith e.Message
+        ProcessPutOrDeleteRequest (BuildUrl server database) "PUT"
     let DeleteDatabase server database =
-        try
-            ProcessPutOrDeleteRequest (BuildUrl server database) "DELETE"
-        with
-        | e -> failwith e.Message
+        ProcessPutOrDeleteRequest (BuildUrl server database) "DELETE"
     let CreateDocument server database content = 
         let jsonContent = JsonConvert.SerializeObject content
         ProcessPostRequest (BuildUrl server database) "POST" jsonContent "application/json"
     let GetDocument<'a> server database documentId =
         let response = ProcessGetRequest ((BuildUrl server database) + "/" + documentId) 
-        JsonConvert.DeserializeObject(response, typeof<'a>) :?> 'a
-    let GetDocuments<'a> server database documentIds =
-        Async.Parallel [for documentId in documentIds -> 
-                            async {return GetDocument<'a> server database documentId }]
-        |> Async.RunSynchronously
+        JsonConvert.DeserializeObject<'a> response
     let GetAllDocuments<'a> server database =
         let jsonObject = ProcessGetRequest ((BuildUrl server database) + "/_all_docs")
                          |> JObject.Parse
@@ -67,7 +57,4 @@ module FSharpCouch
                             async {return JsonConvert.DeserializeObject<'a>(row.ToString())}]
         |> Async.RunSynchronously
     let DeleteDocument server database documentId revision =         
-        try
-            ProcessPutOrDeleteRequest ((BuildUrl server database) + "/" + documentId + "?rev=" + revision) "DELETE"
-        with
-        | e -> failwith e.Message
+        ProcessPutOrDeleteRequest ((BuildUrl server database) + "/" + documentId + "?rev=" + revision) "DELETE"
--- test/FSharpCouch.IntegrationTests/FSharpCouchIntegrationTests.fs
+++ test/FSharpCouch.IntegrationTests/FSharpCouchIntegrationTests.fs
@@ -27,30 +27,23 @@
         let CouchDbServer = "http://localhost:5984"
         let Database = "FSharpCouchTest"
         inherit SpecUnit.ContextSpecification()
-            override x.Because () =
+            override x.CleanUpContext () =
+                FSharpCouch.DeleteDatabase CouchDbServer Database |> ignore
+            override x.Context () =
                 x.fakeRecord <- {_id = Guid.NewGuid().ToString(); SomeValue = "Test"}
                 let fakeRecord2 = {x.fakeRecord with _id = Guid.NewGuid().ToString()}
-                let fakeRecord3 = {x.fakeRecord with _id = Guid.NewGuid().ToString()}
-                try
-                    FSharpCouch.CreateDatabase CouchDbServer Database |> ignore
-                    FSharpCouch.CreateDocument CouchDbServer Database x.fakeRecord |> ignore 
-                    FSharpCouch.CreateDocument CouchDbServer Database fakeRecord2 |> ignore 
-                    FSharpCouch.CreateDocument CouchDbServer Database fakeRecord3 |> ignore 
-                    x.getDocumentResult <- FSharpCouch.GetDocument<TestExistingRecord> CouchDbServer Database x.fakeRecord._id
-                    let documentIds = [x.fakeRecord._id; fakeRecord2._id; fakeRecord3._id]
-                    x.getDocumentsResult <- FSharpCouch.GetDocuments<TestExistingRecord> CouchDbServer Database documentIds
-                    x.getAllDocumentsResult <- FSharpCouch.GetAllDocuments<TestExistingRecord> CouchDbServer Database
-                    System.Threading.Thread.Sleep(100)
-                    FSharpCouch.DeleteDocument CouchDbServer Database x.fakeRecord._id x.getDocumentResult._rev |> ignore
-                finally
-                    System.Threading.Thread.Sleep(100)
-                    FSharpCouch.DeleteDatabase CouchDbServer Database |> ignore
+                let fakeRecord3 = {x.fakeRecord with _id = Guid.NewGuid().ToString()}                
+                FSharpCouch.CreateDatabase CouchDbServer Database |> ignore
+                FSharpCouch.CreateDocument CouchDbServer Database x.fakeRecord |> ignore 
+                FSharpCouch.CreateDocument CouchDbServer Database fakeRecord2 |> ignore 
+                FSharpCouch.CreateDocument CouchDbServer Database fakeRecord3 |> ignore 
+                x.getDocumentResult <- FSharpCouch.GetDocument<TestExistingRecord> CouchDbServer Database x.fakeRecord._id
+                x.getAllDocumentsResult <- FSharpCouch.GetAllDocuments<TestExistingRecord> CouchDbServer Database
+                System.Threading.Thread.Sleep(500)
+                FSharpCouch.DeleteDocument CouchDbServer Database x.fakeRecord._id x.getDocumentResult._rev |> ignore
             [<Test>]    
             member x.should_have_a_document_with_the_expected_id () =    
                 x.getDocumentResult._id.ShouldEqual x.fakeRecord._id |> ignore
-           [<Test>]    
-            member x.should_have_3_documents () =    
-                (Seq.length x.getDocumentsResult).ShouldEqual 3 |> ignore
            [<Test>]    
             member x.should_have_3_documents_from_get_all () =    
                 (Seq.length x.getAllDocumentsResult).ShouldEqual 3 |> ignore
